9a3ac6f94f71d041e55612c2294c5596d27c575e
Miha <stele1000@gmail.com>
2023-04-22 09:24:06 +0200

    Receiver functions

--- receivers/receiverFns.go
+++ receivers/receiverFns.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hi")
 	coord := Coordinate{1, 2}
+	shiftBy(3, 4, &coord)
+	// shiftBy with receiver function
 	coord.shiftBy(3, 4)
 	fmt.Println(&coord)
+	dist := coord.shiftDist(3, 4)
+	fmt.Println(dist)
+	fmt.Println(coord)
 
 }
 
@@ -16,14 +21,20 @@ type Coordinate struct {
 	Y int
 }
 
-//
-//func shiftBy(x, y int, coord *Coordinate) {
-//	coord.X += x
-//	coord.Y += y
-//}
+func shiftBy(x, y int, coord *Coordinate) {
+	coord.X += x
+	coord.Y += y
+}
 
 // Receiver functions
 func (coord *Coordinate) shiftBy(x, y int) {
 	coord.X += x
 	coord.Y += y
+}
+
+// Value receiver - receives a copy of the struct
+func (coord Coordinate) shiftDist(x, y int) Coordinate {
+	coord.X += x
+	coord.Y += y
+	return coord
 }
