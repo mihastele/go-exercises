71694968ec409376efe99c652d96de25d146dcbb
Miha <stele1000@gmail.com>
2023-04-22 22:53:09 +0200

    Type embedding

--- typeembedding/embeddedinerfaces.go
+++ typeembedding/embeddedinerfaces.go
@@ -1,1 +1,39 @@
 package main
+
+// type embedding is a way to reuse the code of an existing type
+// without having to explicitly define all its methods
+// in the new type
+
+type Whisperer interface {
+	Whisper() string
+}
+
+type Yeller interface {
+	Yell() string
+}
+
+type Talker interface {
+	Whisperer
+	Yeller
+}
+
+func talk(t Talker) {
+	println(t.Whisper(), t.Yell())
+}
+
+// Embedded structs
+type Account struct {
+	accountId int
+	balance   int
+	name      string
+}
+
+type ManagerAccount struct {
+	Account
+}
+
+func main() {
+	// Embedded interfaces
+	mgrAcct := ManagerAccount{Account{1, 100, "John"}}
+	println(mgrAcct.accountId, mgrAcct.balance, mgrAcct.name)
+}
