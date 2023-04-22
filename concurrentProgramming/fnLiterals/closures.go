bc1ddb8511ec1d5673bc0b30fbb091e1b7c181ec
Miha <stele1000@gmail.com>
2023-04-22 23:36:44 +0200

    generics.go

---
+++ concurrentProgramming/fnLiterals/closures.go
@@ -0,0 +1,35 @@
+package fnLiterals
+
+import "fmt"
+
+func helloWorld() {
+	fmt.Printf("Hello, ")
+	// anoymous function
+	world := func() {
+		fmt.Printf("world!\n")
+	}
+
+	world()
+	world()
+
+	// Closures
+	// A closure is a function value that references variables from outside its body.
+	// The function may access and assign to the referenced variables; in this sense
+	// the function is "bound" to the variables.
+	// For example, the adder function returns a closure. Each closure is bound to its
+	// own sum variable.
+	outside := 1
+	worldC := func() {
+		outside++
+	}
+
+	// Higher order function
+	world2 := func() func() {
+		return func() {
+			fmt.Printf("world!\n")
+		}
+	}
+
+	worldC()
+	world2()()
+}
--- generics/generics.go
+++ generics/generics.go
@@ -8,6 +8,17 @@ func isEqual[T comparable](a, b T) bool {
 	return a == b
 }
 
+// Type approximation
+//type Integers32 interface {
+//	~int32 | ~uint32
+//}
+
+//type MyInt int32
+//
+//func int32Only[T Integers32](v T) {
+//	println(v)
+//}
+
 func main() {
 	name[int, int](1, 2)
 	name[string, int]("hello", 1)
@@ -17,4 +28,7 @@ func main() {
 	println(isEqual[int](1, 1))
 	println(isEqual[string]("hello", "world"))
 	println(isEqual[string]("hello", "hello"))
+
+	//int32Only[int32](MyInt(1))
+
 }
