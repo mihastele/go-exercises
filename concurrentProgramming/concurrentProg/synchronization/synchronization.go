c5690b8c3b1ed12e7c82c0cf91be78c859a6f233
Miha <stele1000@gmail.com>
2023-04-26 17:16:04 +0200

    defer

--- concurrentProgramming/concurrentProg/synchronization/synchronization.go
+++ concurrentProgramming/concurrentProg/synchronization/synchronization.go
@@ -24,11 +24,34 @@ func (sd *SyncedData) Get(key string) int {
 	return data
 }
 
+// defer  can make sure the mutex gets unlocked
+func (sd *SyncedData) InsertDefer(key string, value int) {
+	sd.mutex.Lock()
+	defer sd.mutex.Unlock()
+	sd.inner[key] = value
+}
+
+func (sd *SyncedData) GetDefer(key string) int {
+	sd.mutex.Lock()
+	defer sd.mutex.Unlock()
+	data := sd.inner[key]
+	return data
+}
+
+//func main() {
+//	data := SyncedData{inner: make(map[string]int)}
+//	data.Insert("a", 1)
+//	data.Insert("b", 2)
+//	go fmt.Println(data.Get("a"))
+//	go fmt.Println(data.Get("b"))
+//	time.Sleep(100 * time.Millisecond)
+//}
+
 func main() {
 	data := SyncedData{inner: make(map[string]int)}
 	data.Insert("a", 1)
 	data.Insert("b", 2)
-	go fmt.Println(data.Get("a"))
-	go fmt.Println(data.Get("b"))
+	fmt.Println(data.Get("a"))
+	fmt.Println(data.Get("b"))
 	time.Sleep(100 * time.Millisecond)
 }
